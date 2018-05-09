481196110cea193b0379d6c166fd1058b96f90f4
Zac Willmington <zac.willmington@gmail.com>
2018-05-09 19:44:32 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -17,7 +17,6 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
       @number = params[:number].to_i
       @phrase = params[:phrase].gsub("%20", " ")
-      binding.pry
         @number.times do
              "#{@phrase}"
         end
