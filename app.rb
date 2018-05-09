845077f1a24ff65ced85d72255bdafbcc44bd71a
Zac Willmington <zac.willmington@gmail.com>
2018-05-09 19:35:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,4 +3,23 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+  get "/reversename/:name" do
+      @name = params[:name]
+       "#{@name.reverse}"
+  end
+
+  get "/square/:number" do
+      @number = params[:number].to_i
+      @squared = @number * @number
+      "#{@squared}"
+  end
+
+  get "/say/:number/:phrase" do
+      @number = params[:number].to_i
+      @phrase = params[:phrase]
+        @number.times do
+            puts "hello"
+        end
+  end
+
 end
