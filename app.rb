72ba0448ce3ce209bc1cb33021b1bfa3bf61da7f
Zac Willmington <zac.willmington@gmail.com>
2018-05-09 20:51:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -42,8 +42,6 @@ class App < Sinatra::Base
 
   get "/:operation/:number1/:number2" do
 
-      erb :sayNtimes
-
       @number1 = params[:number1].to_i
       @number2 = params[:number2].to_i
       @operation = params[:operation]
