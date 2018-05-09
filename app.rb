f4b5b447307af97b390736b62b72b59443b2e70c
Zac Willmington <zac.willmington@gmail.com>
2018-05-09 19:38:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ class App < Sinatra::Base
   get "/say/:number/:phrase" do
       @number = params[:number].to_i
       @phrase = params[:phrase]
-        @number.times do
+        @number.times
              "hello"
         end
   end
